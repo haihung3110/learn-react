9428a13275d9ddcb435048809e1d3e7c29acdd5a
haihung <haihungphandao@gmail.com>
2022-09-01 20:11:13 +0700

    third push

---
+++ demo-react/src/views/Example/MyComponent.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+class MyComponent extends React.Component{
+    render(){
+        let name = 'Ben';
+        
+        return (
+            <div>Welcome to react : {name}</div>
+        )
+    }
+}
+
+export default MyComponent;
